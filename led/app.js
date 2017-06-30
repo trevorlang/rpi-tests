3450ed0bef8d4ef45c73d109ed69cc5e3c952692
Trevor LAng <trevorlang@ymail.com>
2017-06-30 13:07:57 -0500

    Setting up led project

--- led/app.js
+++ led/app.js
@@ -1,24 +1,25 @@
 let Gpio = require('onoff').Gpio;
-let	led = new Gpio(4, 'out');
+let led = new Gpio(4, 'out');
 let interval;
+let led_interval = 50;
 
 console.log("Hello!");
 
 interval = setInterval(function() {
 
 	// Set the new value to be 1 or 0 depending on the previous state
-	let value = ( led.readSync + 1 ) % 2;
+	let value = ( led.readSync() + 1 ) % 2;
 
 	led.write( value, function() {
 		console.log(`--> Changed state to: ${value}`);
 	});
 
-}, 2000);
+}, led_interval);
 
 process.on('SIGINT', function() {
 	clearInterval(interval);
 	led.writeSync(0);
 	led.unexport();
-	console.log("Bye!");
+	console.log("\n\n\n-->App has been closed!\n\n-->Bye!");
 	process.exit();
 });
